b058a2bce8b26c046c9302194066d7e4b4a80c5d
LucasLucky1 <lucaserickson100@gmail.com>
2024-03-30 11:32:59 -0300

    criando primeiro layout

--- app/src/main/java/com/lucky17/Orgs/MainActivity.kt
+++ app/src/main/java/com/lucky17/Orgs/MainActivity.kt
@@ -3,11 +3,14 @@ package com.lucky17.Orgs
 import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this,"Bem vindo ao Android",Toast.LENGTH_LONG).show()
+
+        setContentView(R.layout.activity_main)
     }
 }

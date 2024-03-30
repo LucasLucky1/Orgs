dc1f5a1b1e5e7d8de21b198a7c064ab99630d26e
LucasLucky1 <lucaserickson100@gmail.com>
2024-03-30 10:44:06 -0300

    primeira activity

--- app/src/main/java/com/lucky17/Orgs/MainActivity.kt
+++ app/src/main/java/com/lucky17/Orgs/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.lucky17.Orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
 
 class MainActivity : Activity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this,"Bem vindo ao Android",Toast.LENGTH_LONG).show()
+    }
 }
